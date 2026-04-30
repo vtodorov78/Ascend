8a5df57ce417a3a9cc6172af9f27469da2a3073e
vvtodorov78 <vvtodorov12@gmail.com>
2026-04-30 23:40:20 +0300

    Initial Commit

---
+++ Ascend/App/AscendApp.swift
@@ -0,0 +1,17 @@
+//
+//  AscendApp.swift
+//  Ascend
+//
+//  Created by Vladimir Todorov on 30.04.26.
+//
+
+import SwiftUI
+
+@main
+struct AscendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ascend/Core/Root/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ascend
+//
+//  Created by Vladimir Todorov on 30.04.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
